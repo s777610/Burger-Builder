277f60ed5b741808d35d0d34ee0fefe10abc0b1d
s777610 <s777610@gmail.com>
2018-11-14 20:44:23 -0800

    add burger

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import classes from './Burger.module.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+const burger = (props) => {
+    // Object.keys(props.ingredients) is [salad, bacon, cheese, meat]
+    let transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+        // [...Array(2)] will give me an array with 2 empty spaces(undefined array items), [undefined, undefined]
+        return [...Array(props.ingredients[igKey])].map((_, i) => { // index is important in this case
+            return <BurgerIngredient key={igKey + i} type={igKey} />;
+        });
+    }) // before reduce [[salad, salad], [bacon], [cheese, cheese, cheese], [meat]]
+    .reduce((arr, el) => { // after reduce [salad, salad, bacon, cheese, cheese, cheese, meat]
+        console.log(`arr is ${arr}`) // Above:  salad is <BurgerIngredient .... type={salad} />
+        return arr.concat(el)
+    }, []); // initialValue is []
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>
+    }
+    
+    console.log(transformedIngredients);
+    return (
+        <div className={classes.Burger}>
+            <BurgerIngredient type="bread-top"/>
+            {transformedIngredients}
+            <BurgerIngredient type="bread-bottom"/>
+        </div>
+    );
+}
+
+export default burger;
