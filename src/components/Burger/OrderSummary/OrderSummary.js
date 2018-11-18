7874e4ac3243a5b8b99d744f2c7486cbe8c921c2
s777610 <s777610@gmail.com>
2018-11-18 15:45:42 -0800

    add shouldComponentUpdate and componentWillUpdate in Modal.js

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,31 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import Aux from '../../../hoc/Aux';
+import Aux from '../../../hoc/Aux/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
+// this could be a functional component, doesn't have to be a
+class OrderSummary extends Component {
+    componentWillUpdate() {
+        console.log('[OrderSummary] WillUpdate')
+    }
+
+    render () {
+        const ingredientSummary = Object.keys(this.props.ingredients)
         .map(igKey => { 
             return ( // {textTransform: 'capitalize'} is JS object
             <li key={igKey}>
-                <span style={{textTransform: 'capitalize'}}>{igKey}</span>: <span>{props.ingredients[igKey]}</span>
+                <span style={{textTransform: 'capitalize'}}>{igKey}</span>: <span>{this.props.ingredients[igKey]}</span>
             </li>
             )
         });
 
-    return (
+        return (
         <Aux>
             <h3>Your Order</h3>
             <p>A delicious burger with the following ingredients:</p>
             <ul>
                 {ingredientSummary}
             </ul>
-            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
+            <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
             <p>Contunue to Checkout?</p>
-            <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
+            <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+            <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
         </Aux>
-    )
+        );
+    }
 };
 
-export default orderSummary;
+export default OrderSummary;
