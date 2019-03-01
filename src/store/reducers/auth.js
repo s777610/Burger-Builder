22b61f482939ebcbdb58e5d2f29d2cf4025c1637
s777610 <s777610@gmail.com>
2019-03-01 13:06:11 -0800

    refactor auth reducer

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -12,46 +12,26 @@ const initialState = {
   authRedirectPath: "/"
 };
 
-const authStart = (state, action) => {
-  return updateObject(state, { error: null, loading: true });
-};
-
-const authSuccess = (state, action) => {
-  return updateObject(state, {
-    token: action.idToken,
-    userId: action.userId,
-    error: null,
-    loading: false
-  });
-};
-
-const authFail = (state, action) => {
-  return updateObject(state, {
-    error: action.error,
-    loading: false
-  });
-};
-
-const authLogout = (state, action) => {
-  return updateObject(state, { token: null, userId: null });
-};
-
-const setAuthRedirectPath = (state, action) => {
-  return updateObject(state, { authRedirectPath: action.path }); // set to 'checkout' if user built burger before login
-};
-
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.AUTH_START:
-      return authStart(state, action);
+      return updateObject(state, { error: null, loading: true });
     case actionTypes.AUTH_SUCCESS:
-      return authSuccess(state, action);
+      return updateObject(state, {
+        token: action.idToken,
+        userId: action.userId,
+        error: null,
+        loading: false
+      });
     case actionTypes.AUTH_FAIL:
-      return authFail(state, action);
+      return updateObject(state, {
+        error: action.error,
+        loading: false
+      });
     case actionTypes.AUTH_LOGOUT:
-      return authLogout(state, action);
+      return updateObject(state, { token: null, userId: null });
     case actionTypes.SET_AUTH_REDIRECT_PATH:
-      return setAuthRedirectPath(state, action);
+      return updateObject(state, { authRedirectPath: action.path }); // set to 'checkout' if user built burger before login
     default:
       return state;
   }
