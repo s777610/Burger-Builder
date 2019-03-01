435af9b9db761e549e9b3f58db0ffdc3e6f1d870
s777610 <s777610@gmail.com>
2019-03-01 13:29:49 -0800

    refactor auth action creator

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -36,49 +36,45 @@ export const logout = () => {
   };
 };
 
-export const checkAuthTimeout = expirationTime => {
-  return dispatch => {
-    setTimeout(() => {
-      dispatch(logout());
-    }, expirationTime * 1000);
-  };
+export const checkAuthTimeout = expirationTime => dispatch => {
+  setTimeout(() => {
+    dispatch(logout());
+  }, expirationTime * 1000);
 };
 
-export const auth = (email, password, isSignup) => {
-  return dispatch => {
-    dispatch(authStart());
-    const authData = {
-      email: email,
-      password: password,
-      returnSecureToken: true
-    };
-    let url = `https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=${
+export const auth = (email, password, isSignup) => dispatch => {
+  dispatch(authStart());
+  const authData = {
+    email: email,
+    password: password,
+    returnSecureToken: true
+  };
+  let url = `https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=${
+    process.env.REACT_APP_WEATHER_API_KEY
+  }`;
+  if (!isSignup) {
+    url = `https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=${
       process.env.REACT_APP_WEATHER_API_KEY
     }`;
-    if (!isSignup) {
-      url = `https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=${
-        process.env.REACT_APP_WEATHER_API_KEY
-      }`;
-    }
-    axios
-      .post(url, authData)
-      .then(response => {
-        // console.log(response)
-        const expirationDate = new Date(
-          new Date().getTime() + response.data.expiresIn * 1000
-        );
-        localStorage.setItem("token", response.data.idToken);
-        localStorage.setItem("expirationDate", expirationDate);
-        localStorage.setItem("userId", response.data.localId);
-        // localId is userId
-        dispatch(authSuccess(response.data.idToken, response.data.localId));
-        dispatch(checkAuthTimeout(response.data.expiresIn));
-      })
-      .catch(err => {
-        // console.log(err);
-        dispatch(authFail(err.response.data.error));
-      });
-  };
+  }
+  axios
+    .post(url, authData)
+    .then(response => {
+      // console.log(response)
+      const expirationDate = new Date(
+        new Date().getTime() + response.data.expiresIn * 1000
+      );
+      localStorage.setItem("token", response.data.idToken);
+      localStorage.setItem("expirationDate", expirationDate);
+      localStorage.setItem("userId", response.data.localId);
+      // localId is userId
+      dispatch(authSuccess(response.data.idToken, response.data.localId));
+      dispatch(checkAuthTimeout(response.data.expiresIn));
+    })
+    .catch(err => {
+      // console.log(err);
+      dispatch(authFail(err.response.data.error));
+    });
 };
 
 export const setAuthRedirectPath = path => {
@@ -88,25 +84,23 @@ export const setAuthRedirectPath = path => {
   };
 };
 
-export const authCheckState = () => {
-  return dispatch => {
-    const token = localStorage.getItem("token");
-    if (!token) {
+export const authCheckState = () => dispatch => {
+  const token = localStorage.getItem("token");
+  if (!token) {
+    dispatch(logout());
+  } else {
+    // convert string to Date obj, expirationDate is the time we need to logout
+    const expirationDate = new Date(localStorage.getItem("expirationDate"));
+    if (expirationDate <= new Date()) {
       dispatch(logout());
     } else {
-      // convert string to Date obj, expirationDate is the time we need to logout
-      const expirationDate = new Date(localStorage.getItem("expirationDate"));
-      if (expirationDate <= new Date()) {
-        dispatch(logout());
-      } else {
-        const userId = localStorage.getItem("userId");
-        dispatch(authSuccess(token, userId));
-        dispatch(
-          checkAuthTimeout(
-            (expirationDate.getTime() - new Date().getTime()) / 1000
-          )
-        );
-      }
+      const userId = localStorage.getItem("userId");
+      dispatch(authSuccess(token, userId));
+      dispatch(
+        checkAuthTimeout(
+          (expirationDate.getTime() - new Date().getTime()) / 1000
+        )
+      );
     }
-  };
+  }
 };
